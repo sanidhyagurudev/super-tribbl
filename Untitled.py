11d47dc006b0a081fbdd34c4833ebb66e150ab5e
The_Gurudev_Speaks <64418432+sanidhyagurudev@users.noreply.github.com>
2021-06-01 17:18:41 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import numpy as np
+
+
+# In[8]:
+
+
+kanto = np.array([73, 67, 43])
+
+
+# In[9]:
+
+
+kanto
+
+
+# In[13]:
+
+
+weights = np.array([w1, w2, w3])
+weights
+
+
+# In[14]:
+
+
+get_ipython().system('pip install numpy --upgrade --quite')
+
+
+# In[15]:
+
+
+import numpy as np
+
+
+# In[ ]:
+
+
+kanto = np.array([73, 67, 43]), 
+
+
+# In[16]:
+
+
+kanto
+
+
+# In[20]:
+
+
+weights = np.array([w1, w2, w3]),
+weights
+
+
+# In[18]:
+
+
+type(kanto)
+
+
+# In[22]:
+
+
+
+
+
+# In[24]:
+
+
+type(weights)
+
+
+# In[26]:
+
+
+help(np.dot)
+
+
+# In[28]:
+
+
+import numpy as np
+
+a = np.array(52)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
+
+
+# In[29]:
+
+
+climate_data = np.array([[73, 67, 43],
+                         [91, 88, 64],
+                         [87, 134, 58],
+                         [102, 43, 37],
+                         [69, 96, 70]])
+
+climate_data
+
+
+# In[ ]:
+
+
+
+
